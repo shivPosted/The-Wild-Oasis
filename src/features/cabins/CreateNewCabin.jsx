2f9d841a4c0a6821a498bb611509e0aa59d1db41
shivPosted <shivpratap621@gmail.com>
2024-12-20 22:17:35 +0530

    using new react library react-hook-form

--- src/features/cabins/CreateNewCabin.jsx
+++ src/features/cabins/CreateNewCabin.jsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Textarea from "../../ui/TextArea";
 import FileInput from "../../ui/FileInput";
 import Button from "../../ui/Button";
+import { useForm } from "react-hook-form";
 
 const StyledFormRow = styled.div`
   display: grid;
@@ -42,37 +43,60 @@ const Error = styled.span`
   color: var(--color-red-700);
 `;
 function CreateNewCabin() {
+  const { handleSubmit, register } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <StyledFormRow>
-        <Label for="name">Cabin Name</Label>
-        <Input id="name" type="text" name="name" />
+        <Label htmlFor="name">Cabin Name</Label>
+        <Input id="name" type="text" name="name" {...register("name")} />
       </StyledFormRow>
       <StyledFormRow>
-        <Label for="capacity">Max Capacity</Label>
-        <Input id="capacity" type="number" name="capacity" />
+        <Label htmlFor="capacity">Max Capacity</Label>
+        <Input
+          id="capacity"
+          type="number"
+          name="capacity"
+          {...register("capacity")}
+        />
       </StyledFormRow>
       <StyledFormRow>
-        <Label for="price">Regular Price</Label>
-        <Input id="price" type="number" name="price" />
+        <Label htmlFor="price">Regular Price</Label>
+        <Input id="price" type="number" name="price" {...register("price")} />
       </StyledFormRow>
       <StyledFormRow>
-        <Label for="discount">Discount</Label>
-        <Input id="discount" type="text" name="discount" />
+        <Label htmlFor="discount">Discount</Label>
+        <Input
+          id="discount"
+          type="text"
+          name="discount"
+          {...register("discount")}
+        />
       </StyledFormRow>
       <StyledFormRow>
-        <Label for="description">Description for Cabin</Label>
-        <Textarea id="description" name="description" />
+        <Label htmlFor="description">Description for Cabin</Label>
+        <Textarea
+          id="description"
+          name="description"
+          {...register("description")}
+        />
       </StyledFormRow>
       <StyledFormRow>
-        <Label for="file-input">Image for Cabin</Label>
-        <FileInput id="file-input" name="imageInput" />
+        <Label htmlFor="file-input">Image for Cabin</Label>
+        <FileInput
+          id="file-input"
+          name="imageInput"
+          {...register("fileInput")}
+        />
       </StyledFormRow>
       <StyledFormRow>
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Edit Cabin</Button>
+        <Button>Add Cabin</Button>
       </StyledFormRow>
     </Form>
   );
